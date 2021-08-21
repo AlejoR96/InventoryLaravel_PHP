17f460a86ec43616c26ce406e2929c2078066832
Alejandro Rivera <jarivera636@misena.edu.co>
2021-08-21 10:00:52 -0500

    Se agrego ruta para el botton editar con su respectivo stilos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,9 +12,14 @@ class ProductController extends Controller
         $list = Product::all(); //select * from products
         return view('product/list',['products' => $list]);
     }
-// Controlador para mostrar el formulario
-    function form(){
-        return view('product/form');
+
+// Controlador para mostrar el formulario con los datos boton insertar y editar
+    function form($id =null){
+        $product = new Product();
+        if ($id !=null) {
+            $product = Product::findOrFail($id);
+        }
+        return view('product/form',['product' => $product]);
     }
 
 
@@ -29,8 +34,11 @@ class ProductController extends Controller
         "quantity"=>'required|numeric',
         "brand"=>'required|max:50',
         ]);
-
+                     //Campos del formulario
         $product = new Product();
+        if ($request -> id > 0) {
+            $product = Product::findOrFail($request ->id); #Condicion para actualizar por id
+        }
         $product ->name = $request ->name;
         $product ->cost = $request ->cost;
         $product ->price = $request ->price;
@@ -39,7 +47,7 @@ class ProductController extends Controller
 
         $product ->save();
 
-        return redirect('/products');
+        return redirect('/products')->with('message', 'El producto ha sido guardado/actualizado'); #Variales de sesion
     }
 
     //Controlador para borrar por id los datos
@@ -48,6 +56,6 @@ class ProductController extends Controller
         $product = Product::findOrFail($id);
         $product -> delete();
 
-        return redirect('/products');
+        return redirect('/products')->with('messageDelete', 'El producto ha sido borrado con exito'); #Variales de sesion;
     }
 }
